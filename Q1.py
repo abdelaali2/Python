a9bccec463d3144a3179a85703f088dab2fbaa00
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-03-10 23:47:47 +0200

    Q2 added

---
+++ Q1.py
@@ -0,0 +1,6 @@
+# Q1: Write a Python function that takes a string as input and returns the 
+# string reversed.
+
+def reverseString(input):
+    return input[::-1]
+print(reverseString("Hello World!"))
---
+++ Q2.py
@@ -0,0 +1,20 @@
+# Write a Python program that prompts the user to enter two 
+# numbers and prints out their sum, difference, product, and 
+# quotient.
+# 
+
+print ("Enter the first No.")
+num1 = input()
+num1 = int(num1)
+print ("Enter the Second No.")
+num2 = input()
+num2 = int(num2)
+
+def calculator(x,y):
+    print(f'sum = { x + y }')
+    print(f'sub = { x - y }')
+    print(f'Mul = { x * y }')
+    print(f'Div = { x / y }')
+    print(f'Quotient = { x % y }')
+
+calculator(num1,num2)
