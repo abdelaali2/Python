40da5b991be30614f4276d9d774c275dcd47534f
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-03-11 12:08:47 +0200

    Q4 added

---
+++ Lab 2/Q4.py
@@ -0,0 +1,7 @@
+# Q4: Write a Python program that imports the datetime module with
+# the alias dt and uses the now function to get the current date and
+# time.
+
+import datetime as dt
+
+print(dt.datetime.now())
