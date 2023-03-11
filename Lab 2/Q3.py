5173eaeb0061ca5a3ff51488d437432f8673bf92
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-03-11 12:04:45 +0200

    Q3 added

---
+++ Lab 2/Q3.py
@@ -0,0 +1,7 @@
+# Q3: Write a Python program that imports only the randint function
+# from the random module and uses it to generate a random integer
+# between 1 and 10.
+
+from random import randint
+
+print(randint(1,10))
