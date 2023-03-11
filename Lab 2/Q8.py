e53c2c62aee8d94165d6ddbc41f31bc31a945496
Ibrahim Badr <hamedb.ibrahim@gmail.com>
2023-03-11 12:54:48 +0200

    Q8 added

---
+++ Lab 2/Q8.py
@@ -0,0 +1,12 @@
+# Write a Python program that reads a file named example.txt and
+# writes its contents to a new file named copy. txt.
+
+destFile = open("Lab 2\Q8_Copy.txt", "w")
+
+with open("Lab 2\Q8_Example.txt") as srcFile:
+    destFile.writelines(srcFile.read() + "\n Copied by Ibrahim Badr")
+
+destFile.close()
+
+with open("Lab 2\Q8_Copy.txt") as myFile:
+    print(myFile.read())
